7ed67aa8786329bf416285cadbb13b3ae9c3981d
Cayo M Amorim <cayomesquita@gmail.com>
2020-05-09 10:56:11 -0700

    Initial project

---
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Cornerstone on 2020-05-09.
+//  Copyright © 2020 Cornerstone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
