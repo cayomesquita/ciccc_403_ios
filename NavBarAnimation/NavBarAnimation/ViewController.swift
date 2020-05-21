91dee1bcb63cc0337fce5ff8c15c95682a5e4847
Cayo M Amorim <cayomesquita@gmail.com>
2020-05-21 10:55:34 -0700

    Initial Project

---
+++ NavBarAnimation/NavBarAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavBarAnimation
+//
+//  Created by Cornerstone on 2020-05-21.
+//  Copyright © 2020 Cornerstone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
