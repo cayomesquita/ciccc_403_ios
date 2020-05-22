c2ef67cc0950365abd280274230b528ee3d6ea56
Cayo M Amorim <cayomesquita@gmail.com>
2020-05-21 21:48:54 -0700

    Add navBar+plusButton animation

--- NavBarAnimation/NavBarAnimation/ViewController.swift
+++ NavBarAnimation/NavBarAnimation/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var navBarHeight: NSLayoutConstraint!
+    @IBOutlet var plusButton: UIButton!
+    
+    var initialHeight:CGFloat = CGFloat.init()
+    let endHeight = 200
+    
+    var isOpen: Bool {
+        return self.navBarHeight.constant == CGFloat(endHeight)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        initialHeight = navBarHeight.constant
     }
-
-
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        let closing = isOpen
+        self.navBarHeight.constant = closing ? CGFloat(initialHeight) : CGFloat(endHeight)
+        UIView.animate(withDuration: 2,
+                       delay: 0,
+                       usingSpringWithDamping: 0.25, initialSpringVelocity: 1,
+                       options: .curveEaseInOut,
+                       animations: {
+                        self.view.layoutIfNeeded()
+                        self.plusButton.transform = CGAffineTransform.init(rotationAngle: closing ? 0 : .pi/4)
+                        }
+        )
+    }
+    
 }
 
