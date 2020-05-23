a80b73e12cd6b0bd5ec96e920b804cbe5f3b3865
Cayo M Amorim <cayomesquita@gmail.com>
2020-05-23 12:26:39 -0700

    Add snack adding

--- NavBarAnimation/NavBarAnimation/ViewController.swift
+++ NavBarAnimation/NavBarAnimation/ViewController.swift
@@ -6,11 +6,11 @@ class ViewController: UIViewController {
     @IBOutlet var navBarHeight: NSLayoutConstraint!
     @IBOutlet var plusButton: UIButton!
     
-    let oreoImage = UIImageView.init(image: UIImage.init(named: "oreos"))
-    let pizzaImage = UIImageView.init(image: UIImage.init(named: "pizza_pockets"))
-    let popTartsImage = UIImageView.init(image: UIImage.init(named: "pop_tarts"))
-    let popSicleImage = UIImageView.init(image: UIImage.init(named: "popsicle"))
-    let ramenImage = UIImageView.init(image: UIImage.init(named: "ramen"))
+    let oreoButton = UIButton.init(type: .custom)
+    let pizzaButton = UIButton.init(type: .custom)
+    let popTartsButton = UIButton.init(type: .custom)
+    let popSicleButton = UIButton.init(type: .custom)
+    let ramenButton = UIButton.init(type: .custom)
     
     var stack: UIStackView!
     
@@ -19,17 +19,19 @@ class ViewController: UIViewController {
     var initialHeight:CGFloat = CGFloat.init()
     let endHeight:CGFloat = 200
     
+    var snacks = [String]()
+    
     var state: State = .closed
-//    {
-//        didSet (newValue) {
-//            switch newValue {
-//            case .opened:
-//                self.stack.isHidden = false
-//            case .closed:
-//                self.stack.isHidden = true
-//            }
-//        }
-//    }
+    //    {
+    //        didSet (newValue) {
+    //            switch newValue {
+    //            case .opened:
+    //                self.stack.isHidden = false
+    //            case .closed:
+    //                self.stack.isHidden = true
+    //            }
+    //        }
+    //    }
     
     var isOpen: Bool { return state == .opened }
     
@@ -47,7 +49,24 @@ class ViewController: UIViewController {
     }
     
     func setupView() -> Void {
-        self.stack = UIStackView.init(arrangedSubviews: [oreoImage, pizzaImage, popTartsImage, popSicleImage, ramenImage])
+        self.oreoButton.setImage(UIImage.init(named: "oreos"), for: .normal)
+        self.oreoButton.tag = 0
+        self.pizzaButton.setImage(UIImage.init(named: "pizza_pockets"), for: .normal)
+        self.pizzaButton.tag = 1
+        self.popTartsButton.setImage(UIImage.init(named: "pop_tarts"), for: .normal)
+        self.popTartsButton.tag = 2
+        self.popSicleButton.setImage(UIImage.init(named: "popsicle"), for: .normal)
+        self.popSicleButton.tag = 3
+        self.ramenButton.setImage(UIImage.init(named: "ramen"), for: .normal)
+        self.ramenButton.tag = 4
+        
+        self.oreoButton.addTarget(self, action: #selector(self.snackButtonTapped), for: .touchUpInside)
+        self.pizzaButton.addTarget(self, action: #selector(self.snackButtonTapped), for: .touchUpInside)
+        self.popTartsButton.addTarget(self, action: #selector(self.snackButtonTapped), for: .touchUpInside)
+        self.popSicleButton.addTarget(self, action: #selector(self.snackButtonTapped), for: .touchUpInside)
+        self.ramenButton.addTarget(self, action: #selector(self.snackButtonTapped), for: .touchUpInside)
+        
+        self.stack = UIStackView.init(arrangedSubviews: [oreoButton, pizzaButton, popTartsButton, popSicleButton, ramenButton])
         self.stack.translatesAutoresizingMaskIntoConstraints = false
         self.stack.axis = .horizontal
         self.stack.distribution = .fillEqually
@@ -58,6 +77,9 @@ class ViewController: UIViewController {
         
         self.tableView = UITableView.init()
         self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
         self.view.addSubview(self.tableView)
     }
@@ -65,18 +87,18 @@ class ViewController: UIViewController {
     func setupLayout() -> Void {
         
         let heightImage = CGFloat.init(100)
-                
+        
         NSLayoutConstraint.activate([
             //navbar
             self.stack.bottomAnchor.constraint(equalTo: self.navBar.bottomAnchor, constant: -5),
             self.stack.leadingAnchor.constraint(equalTo: self.navBar.leadingAnchor, constant: 5),
             self.stack.trailingAnchor.constraint(equalTo: self.plusButton.leadingAnchor, constant: -5),
             self.stack.heightAnchor.constraint(lessThanOrEqualTo: self.navBar.heightAnchor, multiplier: 0.8),
-            self.oreoImage.heightAnchor.constraint(lessThanOrEqualToConstant: heightImage),
-            self.pizzaImage.heightAnchor.constraint(lessThanOrEqualToConstant: heightImage),
-            self.popTartsImage.heightAnchor.constraint(lessThanOrEqualToConstant: heightImage),
-            self.popSicleImage.heightAnchor.constraint(lessThanOrEqualToConstant: heightImage),
-            self.ramenImage.heightAnchor.constraint(lessThanOrEqualToConstant: heightImage),
+            self.oreoButton.heightAnchor.constraint(lessThanOrEqualToConstant: heightImage),
+            self.pizzaButton.heightAnchor.constraint(lessThanOrEqualToConstant: heightImage),
+            self.popTartsButton.heightAnchor.constraint(lessThanOrEqualToConstant: heightImage),
+            self.popSicleButton.heightAnchor.constraint(lessThanOrEqualToConstant: heightImage),
+            self.ramenButton.heightAnchor.constraint(lessThanOrEqualToConstant: heightImage),
             
             // tableView
             self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
@@ -87,12 +109,30 @@ class ViewController: UIViewController {
         
     }
     
+    @objc func snackButtonTapped(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            snacks.append("Oreo")
+        case 1:
+            snacks.append("Pizza Pocket")
+        case 2:
+            snacks.append("Pop Tarts")
+        case 3:
+            snacks.append("Popsicle")
+        case 4:
+            snacks.append("Ramen")
+        default:
+            break
+        }
+        tableView.insertRows(at: [IndexPath.init(row: snacks.count - 1, section: 0)], with: .automatic)
+    }
+    
     @IBAction func plusButtonTapped(_ sender: UIButton) {
         state = (state == .opened) ? .closed : .opened
         self.navBarHeight.constant = self.isOpen ? CGFloat(endHeight) : CGFloat(initialHeight)
-        UIView.animate(withDuration: 2,
+        UIView.animate(withDuration: 1.5,
                        delay: 0,
-                       usingSpringWithDamping: 0.25, initialSpringVelocity: 1,
+                       usingSpringWithDamping: 0.5, initialSpringVelocity: 1,
                        options: .curveEaseInOut,
                        animations: {
                         self.view.layoutIfNeeded()
@@ -101,6 +141,26 @@ class ViewController: UIViewController {
         }
         )
         
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
+
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.snacks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = self.snacks[indexPath.row]
+        return cell
     }
     
 }
